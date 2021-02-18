e44f33a7fa9792496ad20100e2b6c7919a10707f
AlvinAhmadov <alvin.dev.ahmadov@gmail.com>
2021-02-18 07:21:12 +0400

    Update

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 var {Predictor} = require("./src/predictor.js");
 
-async function initModule(type, params, shardsPrefix) {
-	try {
-		return new Predictor(params, false, type, shardsPrefix);
-	} catch (err) {
-		console.error('err23904094', err)
-		throw Error(err);
-	}
-}
-
-
 module.exports = {
-	initModule,
 	Predictor
-}
+};
